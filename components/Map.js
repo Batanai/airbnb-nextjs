c491a84d6d4a36a67e59e31bb219de83b18d49c8
Batanai <batiemc@gmail.com>
2021-08-29 12:13:41 +0200

    mapbox

---
+++ components/Map.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+const Map = ({ searchResults }) => {
+  const [selectedLocation, setSelectedLocation] = useState({});
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latidude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 51.5150422,
+    longitude: -0.1108376,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/batanai/cksn5kqof0klq17pqxhtqeppc"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            latitude={result.lat}
+            longitude={result.long}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
+            >
+              🏨{" "}
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+};
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYmF0YW5haSIsImEiOiJja3NuNWFua2EwYTFoMnBvNjg3a2RmczNwIn0.7yHAoUTYbOhiuFbEd0Mpzw",
+  },
 };
